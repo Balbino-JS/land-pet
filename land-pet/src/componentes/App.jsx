8760fe86bdd6f3bc290e355db076e5fd9d00ea7a
Balbino-teste <balbinojteste@gmail.com>
2025-08-15 22:07:09 -0300

    Nessa seção foi criado a navbar da página também foi corrigido alguns erros de digitação

--- land-pet/src/componentes/App.jsx
+++ land-pet/src/componentes/App.jsx
@@ -7,9 +7,6 @@ function App() {
   return (
     <div className='home-container'>
       
-
-
-
    </div>
   )
 }
--- land-pet/src/componentes/Navbar.jsx
+++ land-pet/src/componentes/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import Logo from "../assets/logo.jpg"
-import HomeIcon from "@mui/icon-material/home"
+import HomeIcon from "@mui/icons-material/Home"
 import InfoIcon from "@mui/icons-material/Info"
 import HomeWorkIcon from "@mui/icons-material/HomeWork"
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded"
 import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded"
-import HiOutlineBars3 from "react-icon/hi2"
+import { HiOutlineBars3 } from "react-icons/hi2"
 import Drawer from "@mui/material/Drawer"
 import Box from "@mui/material/Box"
 import List from "@mui/material/List"
@@ -16,77 +16,65 @@ import ListItemText from "@mui/material/ListItemText"
 
 function Navbar() {
   const [openMenu, setOpenMenu] = useState(false)
-  const menuOptins =[
-    {
-        text:"Home",
-        icon: <HomeIcon />,
-    },
-    {
-        text: "Sobre Nós",
-        icon: <InfoIcon />,
-    },
-    {
-        text: "Nosso trabalho",
-        icon: <HomeWorkIcon />,
-    },
-    {
-        text: "Avaiações",
-        icon: <CommentRoundedIcon />,
-    },
-    {
-        text: "Contato",
-        icon: <PhoneRoundedIcon />,
-    }
+
+  const menuOptions = [
+    { text: "Home", icon: <HomeIcon /> },
+    { text: "Sobre Nós", icon: <InfoIcon /> },
+    { text: "Nosso trabalho", icon: <HomeWorkIcon /> },
+    { text: "Avaliações", icon: <CommentRoundedIcon /> },
+    { text: "Contato", icon: <PhoneRoundedIcon /> }
   ]
+
   return (
-    <div className='home-container'>
+    <div className="home-container">
       <nav>
         <div className="nav-logo-container">
-            <img src={Logo} alt="logo pet shop" style={{width: "100px", height: "auto",borderRadius: "10px"}} />
-            <span className="texto-sobre-imagem">Pet Shop</span>
+          <img
+            src={Logo}
+            alt="logo pet shop"
+            style={{ width: "75px", height: "auto", borderRadius: "1rem" }}
+          />
+          <span className="texto-sobre-imagem">Pet Shop</span>
         </div>
 
-        <div className="navbar-links-contatiner">
-            <a href="">Home</a>
-            <a href="">Sobre Nós</a>
-            <a href="">Nosso trabalho</a>
-            <a href="">Avaliações</a>
-            <a href="">Contato</a>
-            <button className="primary-button">Fale conosco</button>            
+        <div className="navbar-links-container">
+          <a href="">Home</a>
+          <a href="">Sobre Nós</a>
+          <a href="">Nosso trabalho</a>
+          <a href="">Avaliações</a>
+          <a href="">Contato</a>
+          <button className="primary-button">Fale conosco</button>
         </div>
 
         <div className="navbar-menu-container">
-            <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+          <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
         </div>
 
-        <Drawer 
-        open={openMenu} 
-        onClose={() => 
-        setOpenMenu(false)} 
-        anchor="rigth">
-            <Box
-                sx={{width: 250}}
-                role="presenttion"
-                onClick={() => setOpenMenu(flse)}
-                onKeyDown={() => setOpenMenu(false)}>
-                    <List>
-                        {menuOptins.map((item) => (
-                            <ListItem key={item.text} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>{item.icon}</ListItemIcon>
-                                    <ListItemText primary={item.text}/>
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                    </List>
-            </Box>
+        <Drawer
+          open={openMenu}
+          onClose={() => setOpenMenu(false)}
+          anchor="right"
+        >
+          <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={() => setOpenMenu(false)}
+            onKeyDown={() => setOpenMenu(false)}
+          >
+            <List>
+              {menuOptions.map((item) => (
+                <ListItem key={item.text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
         </Drawer>
-
-
       </nav>
-
-
-   </div>
+    </div>
   )
 }
 
