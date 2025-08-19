6d16a774d22b9510e3e958276edf4f8d4fc43b13
Balbino-teste <balbinojteste@gmail.com>
2025-08-18 21:45:17 -0300

    Seção de criação do conteúdo da vitrine

--- land-pet/src/componentes/Work.jsx
+++ land-pet/src/componentes/Work.jsx
@@ -1,17 +1,113 @@
-import { useState } from 'react'
-import '../CSS/App.css'
+import { useState } from "react";
+import "../CSS/App.css";
 
-function Work () {
-
-    return(
-        <div className='work-container'>
-            <div className="work-section-container">
-                <h1 className="primary-work">Nosso Trabalho</h1>
-                <p>Sabemos que seu pet é parte da sua família. Deixá-lo para viajar ou em uma ocasião especial pode ser uma preocupação, mas não precisa ser. Nossa hospedagem foi criada para oferecer mais do que um espaço seguro: é um ambiente acolhedor onde seu cão ou gato recebe carinho, atenção e se diverte enquanto você está fora.</p>
-            </div>
-
+function Work() {
+  return (
+    <div className="work-pai-container">
+      <div className="work-container">
+        <div className="work-section-container">
+          <div className="work-section-text">
+            <h1 className="primary-work">Nosso Trabalho</h1>
+            <p className="work-text">
+              Sabemos que seu pet é parte da sua família. Deixá-lo para viajar
+              ou em uma ocasião especial pode ser uma preocupação, mas não
+              precisa ser. Nossa hospedagem foi criada para oferecer mais do que
+              um espaço seguro: é um ambiente acolhedor onde seu cão ou gato
+              recebe carinho, atenção e se diverte enquanto você está fora.
+            </p>
+          </div>
         </div>
-    )
+      </div>
+
+        <h2 className="vitrine-work">Vitrine</h2>
+
+      <div class="container">
+
+      
+
+        <div class="card">
+            
+          <div class="content">
+            <h2>01</h2>
+            <h3>Gatos</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
+              minus dolorum hic aliquid suscipit ipsa commodi, earum voluptatum
+              iure fugit, saepe eos, magnam sequi facere doloribus quos
+              excepturi velit. Eaque!
+            </p>
+            <a href="#">Mais Informação</a>
+          </div>
+        </div>
+        <div class="card">
+          <div class="content">
+            <h2>02</h2>
+            <h3>Cães de pequeno e medio porte</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
+              minus dolorum hic aliquid suscipit ipsa commodi, earum voluptatum
+              iure fugit, saepe eos, magnam sequi facere doloribus quos
+              excepturi velit. Eaque!
+            </p>
+            <a href="#">Mais Informação</a>
+          </div>
+        </div>
+        <div class="card">
+          <div class="content">
+            <h2>03</h2>
+            <h3>Cães de grande porte</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
+              minus dolorum hic aliquid suscipit ipsa commodi, earum voluptatum
+              iure fugit, saepe eos, magnam sequi facere doloribus quos
+              excepturi velit. Eaque!.
+            </p>
+            <a href="#">Mais Informação</a>
+          </div>
+        </div>
+         <div class="card">
+          <div class="content">
+            <h2>03</h2>
+            <h3>Cães de grande porte</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
+              minus dolorum hic aliquid suscipit ipsa commodi, earum voluptatum
+              iure fugit, saepe eos, magnam sequi facere doloribus quos
+              excepturi velit. Eaque!.
+            </p>
+            <a href="#">Mais Informação</a>
+          </div>
+        </div>
+         <div class="card">
+          <div class="content">
+            <h2>03</h2>
+            <h3>Cães de grande porte</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
+              minus dolorum hic aliquid suscipit ipsa commodi, earum voluptatum
+              iure fugit, saepe eos, magnam sequi facere doloribus quos
+              excepturi velit. Eaque!.
+            </p>
+            <a href="#">Mais Informação</a>
+          </div>
+        </div>
+         <div class="card">
+          <div class="content">
+            <h2>03</h2>
+            <h3>Cães de grande porte</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
+              minus dolorum hic aliquid suscipit ipsa commodi, earum voluptatum
+              iure fugit, saepe eos, magnam sequi facere doloribus quos
+              excepturi velit. Eaque!.
+            </p>
+            <a href="#">Mais Informação</a>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
 }
 
-export default Work
+export default Work;
