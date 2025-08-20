d9d81f2aedbf491b2e84377d808a13f314a45edd
Balbino-teste <balbinojteste@gmail.com>
2025-08-20 17:30:57 -0300

    Foi adicionado o servidor para armazenar as avaliações dos usuários

---
+++ API/server.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import cors from 'cors'
+import pkg from './generated/prisma/index.js'; 
+import { ObjectId } from 'bson'; 
+
+
+const { PrismaClient } = pkg;
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+/* Salvar os usuários */
+app.post('/usuarios', async (req, res) => {
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name: req.body.name,
+                testimony: req.body.testimony
+                }
+        });
+
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao salvar usuário.', detalhes: error.message });
+    }
+});
+
+/* Listar usuários */
+app.get('/usuarios', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar usuários.', detalhes: error.message })
+    }
+})
+
+// autera a de usuários
+
+app.put('/usuarios/:id', async (req, res) => {
+    try {
+        const user = await prisma.user.update({
+            where:{
+                id: req.params.id
+            },
+            data: {
+                name: req.body.name,
+                testimony: req.body.testimony
+            }
+        })
+
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao salvar usuário.', detalhes: error.message });
+    }
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        await prisma.user.delete({
+            where: {
+                id: new ObjectId(id) 
+            }
+        });
+
+        res.status(200).json({ message: 'Usuário deletado com sucesso!' });
+    } catch (error) {
+        console.error('Erro ao deletar usuário:', error);
+        res.status(500).json({ error: 'Erro ao deletar usuário.', detalhes: error.message });
+    }
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
