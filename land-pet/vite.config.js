652437b0623bb6bdd18e28fa9bb9e726235e5462
Balbino-teste <balbinojteste@gmail.com>
2025-08-22 11:15:38 -0300

    Configuração de deploy

--- land-pet/vite.config.js
+++ land-pet/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/land-pet/",
+  base: "/land-pet",
   server: {
     open: true, 
     hmr: {
